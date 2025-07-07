29452639f0ec10de4d895234330a43256324fd4b
playergame125 <matteorva05@gmail.com>
2025-07-07 22:14:36 +0200

    funzionante con emacs

--- build/CMakeFiles/fluidssimulator.dir/main.cpp.o.d
+++ build/CMakeFiles/fluidssimulator.dir/main.cpp.o.d
@@ -1,13 +1,36 @@
 CMakeFiles/fluidssimulator.dir/main.cpp.o: \
  /home/playergame125/Desktop/progetti/c++/simple_fluid_simulation/main.cpp \
- /usr/include/stdc-predef.h /usr/include/c++/15.1.1/iostream \
- /usr/include/c++/15.1.1/bits/requires_hosted.h \
- /usr/include/c++/15.1.1/x86_64-pc-linux-gnu/bits/c++config.h \
- /usr/include/c++/15.1.1/x86_64-pc-linux-gnu/bits/os_defines.h \
+ /usr/include/stdc-predef.h /usr/include/SDL3/SDL_video.h \
+ /usr/include/SDL3/SDL_stdinc.h /usr/include/SDL3/SDL_platform_defines.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/include/stdarg.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
  /usr/include/features.h /usr/include/features-time64.h \
  /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
  /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
  /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h /usr/include/string.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/include/stddef.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/include/wchar.h /usr/include/bits/floatn.h \
+ /usr/include/bits/floatn-common.h /usr/include/bits/types/wint_t.h \
+ /usr/include/bits/types/mbstate_t.h \
+ /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/SDL3/SDL_begin_code.h \
+ /usr/include/SDL3/SDL_close_code.h /usr/include/SDL3/SDL_error.h \
+ /usr/include/SDL3/SDL_pixels.h /usr/include/SDL3/SDL_endian.h \
+ /usr/include/endian.h /usr/include/bits/endian.h \
+ /usr/include/bits/endianness.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/uintn-identity.h /usr/include/SDL3/SDL_properties.h \
+ /usr/include/SDL3/SDL_rect.h /usr/include/SDL3/SDL_surface.h \
+ /usr/include/SDL3/SDL_blendmode.h /usr/include/SDL3/SDL_iostream.h \
+ /usr/include/c++/15.1.1/iostream \
+ /usr/include/c++/15.1.1/bits/requires_hosted.h \
+ /usr/include/c++/15.1.1/x86_64-pc-linux-gnu/bits/c++config.h \
+ /usr/include/c++/15.1.1/x86_64-pc-linux-gnu/bits/os_defines.h \
  /usr/include/c++/15.1.1/x86_64-pc-linux-gnu/bits/cpu_defines.h \
  /usr/include/c++/15.1.1/pstl/pstl_config.h \
  /usr/include/c++/15.1.1/ostream /usr/include/c++/15.1.1/bits/ostream.h \
@@ -15,15 +38,7 @@ CMakeFiles/fluidssimulator.dir/main.cpp.o: \
  /usr/include/c++/15.1.1/bits/stringfwd.h \
  /usr/include/c++/15.1.1/bits/memoryfwd.h \
  /usr/include/c++/15.1.1/bits/postypes.h /usr/include/c++/15.1.1/cwchar \
- /usr/include/wchar.h /usr/include/bits/libc-header-start.h \
- /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/include/stddef.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/include/stdarg.h \
- /usr/include/bits/wchar.h /usr/include/bits/types/wint_t.h \
- /usr/include/bits/types/mbstate_t.h \
- /usr/include/bits/types/__mbstate_t.h /usr/include/bits/types/__FILE.h \
- /usr/include/bits/types/FILE.h /usr/include/bits/types/locale_t.h \
- /usr/include/bits/types/__locale_t.h /usr/include/c++/15.1.1/exception \
+ /usr/include/c++/15.1.1/exception \
  /usr/include/c++/15.1.1/bits/exception.h \
  /usr/include/c++/15.1.1/bits/version.h \
  /usr/include/c++/15.1.1/bits/exception_ptr.h \
@@ -38,10 +53,7 @@ CMakeFiles/fluidssimulator.dir/main.cpp.o: \
  /usr/include/c++/15.1.1/x86_64-pc-linux-gnu/bits/c++locale.h \
  /usr/include/c++/15.1.1/clocale /usr/include/locale.h \
  /usr/include/bits/locale.h /usr/include/c++/15.1.1/cctype \
- /usr/include/ctype.h /usr/include/bits/types.h \
- /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
- /usr/include/bits/endian.h /usr/include/bits/endianness.h \
- /usr/include/c++/15.1.1/bits/ios_base.h \
+ /usr/include/ctype.h /usr/include/c++/15.1.1/bits/ios_base.h \
  /usr/include/c++/15.1.1/ext/atomicity.h \
  /usr/include/c++/15.1.1/x86_64-pc-linux-gnu/bits/gthr.h \
  /usr/include/c++/15.1.1/x86_64-pc-linux-gnu/bits/gthr-default.h \
@@ -107,9 +119,7 @@ CMakeFiles/fluidssimulator.dir/main.cpp.o: \
  /usr/include/c++/15.1.1/ext/string_conversions.h \
  /usr/include/c++/15.1.1/cstdlib /usr/include/stdlib.h \
  /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
- /usr/include/sys/types.h /usr/include/bits/stdint-intn.h \
- /usr/include/endian.h /usr/include/bits/byteswap.h \
- /usr/include/bits/uintn-identity.h /usr/include/sys/select.h \
+ /usr/include/sys/types.h /usr/include/sys/select.h \
  /usr/include/bits/select.h /usr/include/bits/types/sigset_t.h \
  /usr/include/alloca.h /usr/include/bits/stdlib-float.h \
  /usr/include/c++/15.1.1/bits/std_abs.h /usr/include/c++/15.1.1/cstdio \
@@ -145,22 +155,12 @@ CMakeFiles/fluidssimulator.dir/main.cpp.o: \
  /usr/include/c++/15.1.1/bits/basic_ios.tcc \
  /usr/include/c++/15.1.1/bits/ostream.tcc /usr/include/c++/15.1.1/istream \
  /usr/include/c++/15.1.1/bits/istream.tcc /usr/include/SDL3/SDL.h \
- /usr/include/SDL3/SDL_stdinc.h /usr/include/SDL3/SDL_platform_defines.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/15.1.1/include/stdint.h \
- /usr/include/stdint.h /usr/include/bits/stdint-uintn.h \
- /usr/include/bits/stdint-least.h /usr/include/string.h \
- /usr/include/strings.h /usr/include/SDL3/SDL_begin_code.h \
- /usr/include/SDL3/SDL_close_code.h /usr/include/SDL3/SDL_assert.h \
- /usr/include/SDL3/SDL_asyncio.h /usr/include/SDL3/SDL_atomic.h \
- /usr/include/SDL3/SDL_audio.h /usr/include/SDL3/SDL_endian.h \
- /usr/include/SDL3/SDL_error.h /usr/include/SDL3/SDL_mutex.h \
- /usr/include/SDL3/SDL_thread.h /usr/include/SDL3/SDL_properties.h \
- /usr/include/SDL3/SDL_iostream.h /usr/include/SDL3/SDL_bits.h \
- /usr/include/SDL3/SDL_blendmode.h /usr/include/SDL3/SDL_camera.h \
- /usr/include/SDL3/SDL_pixels.h /usr/include/SDL3/SDL_surface.h \
- /usr/include/SDL3/SDL_rect.h /usr/include/SDL3/SDL_clipboard.h \
- /usr/include/SDL3/SDL_cpuinfo.h /usr/include/SDL3/SDL_dialog.h \
- /usr/include/SDL3/SDL_video.h /usr/include/SDL3/SDL_events.h \
+ /usr/include/SDL3/SDL_assert.h /usr/include/SDL3/SDL_asyncio.h \
+ /usr/include/SDL3/SDL_atomic.h /usr/include/SDL3/SDL_audio.h \
+ /usr/include/SDL3/SDL_mutex.h /usr/include/SDL3/SDL_thread.h \
+ /usr/include/SDL3/SDL_bits.h /usr/include/SDL3/SDL_camera.h \
+ /usr/include/SDL3/SDL_clipboard.h /usr/include/SDL3/SDL_cpuinfo.h \
+ /usr/include/SDL3/SDL_dialog.h /usr/include/SDL3/SDL_events.h \
  /usr/include/SDL3/SDL_gamepad.h /usr/include/SDL3/SDL_guid.h \
  /usr/include/SDL3/SDL_joystick.h /usr/include/SDL3/SDL_power.h \
  /usr/include/SDL3/SDL_sensor.h /usr/include/SDL3/SDL_keyboard.h \
